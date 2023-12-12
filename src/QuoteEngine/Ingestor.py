3cf0e7f09271ef4372db48dedfbc2e4aee8807d9
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 13:19:41 +0100

    Add docstrings.

--- src/QuoteEngine/Ingestor.py
+++ src/QuoteEngine/Ingestor.py
@@ -9,10 +9,23 @@ from .TextIngestor import TextIngestor
 
 
 class Ingestor(IngestorInterface):
+    """
+    Handles the ingestion of different file types and parses them into QuoteModel objects.
+    """
+
     ingestors = [DocxIngestor, CSVIngestor, TextIngestor, PDFIngestor]
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
+        """
+        Parses the given file at the specified path and returns QuoteModel objects.
+
+        Args:
+            path (str): The path to the file to be parsed.
+
+        Returns:
+            List[QuoteModel]: A list of QuoteModel objects parsed from the file.
+        """
         for ingestor in cls.ingestors:
             if ingestor.can_ingest(path):
                 return ingestor.parse(path)
--- src/QuoteEngine/IngestorInterface.py
+++ src/QuoteEngine/IngestorInterface.py
@@ -5,14 +5,43 @@ from .QuoteModel import QuoteModel
 
 
 class IngestorInterface(ABC):
+    """
+    An abstract base class for ingesting different types of files and extracting quotes.
+
+    Attributes:
+        ingestable_extensions (List[str]): File extensions that can be ingested.
+
+    Methods:
+        can_ingest(path: str) -> bool: Checks if a file with the given path can be ingested.
+        parse(path: str) -> List[QuoteModel]: Parses a file with the given path and QuoteModel objects.
+    """
+
     ingestable_extensions = []
 
     @classmethod
     def can_ingest(cls, path: str) -> bool:
+        """
+        Checks if a file with the given path can be ingested.
+
+        Args:
+            path (str): The path of the file to check.
+
+        Returns:
+            bool: True if the file can be ingested, False otherwise.
+        """
         file_extension = path.split('.')[-1]
         return file_extension in cls.ingestable_extensions
 
     @classmethod
     @abstractmethod
     def parse(cls, path: str) -> List[QuoteModel]:
+        """
+        Parses a file with the given path and returns a list of QuoteModel objects.
+
+        Args:
+            path (str): The path of the file to parse.
+
+        Returns:
+            List[QuoteModel]: A list of QuoteModel objects extracted from the file.
+        """
         pass
