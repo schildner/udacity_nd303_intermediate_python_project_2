569888804e96c54d415a3a434b9c9e397d38f710
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 12:14:36 +0100

    Temp run script to debug - remove later.

---
+++ src/run.py
@@ -0,0 +1,6 @@
+from QuoteEngine import Ingestor
+
+print(Ingestor.parse('./_data/DogQuotes/DogQuotesCSV.csv'))
+print(Ingestor.parse('./_data/DogQuotes/DogQuotesDOCX.docx'))
+print(Ingestor.parse('./_data/DogQuotes/DogQuotesPDF.pdf'))
+print(Ingestor.parse('./_data/DogQuotes/DogQuotesTXT.txt'))
