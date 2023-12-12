8b00487f3391a6982ed3f33313b878fb6ead1ad6
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 13:08:27 +0100

    Adapt the test temp script.

--- src/run.py
+++ src/run.py
@@ -1,6 +1,11 @@
 from QuoteEngine import Ingestor
 
+print('Testing Ingestor.parse()')
+print('CSV...')
 print(Ingestor.parse('./_data/DogQuotes/DogQuotesCSV.csv'))
+print('Docx...')
 print(Ingestor.parse('./_data/DogQuotes/DogQuotesDOCX.docx'))
+print('PDF...')
 print(Ingestor.parse('./_data/DogQuotes/DogQuotesPDF.pdf'))
+print('TXT...')
 print(Ingestor.parse('./_data/DogQuotes/DogQuotesTXT.txt'))
