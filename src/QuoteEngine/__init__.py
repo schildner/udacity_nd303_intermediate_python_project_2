a987df2f85066172b9024db7bbf9e10ad24c80fc
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 12:04:41 +0100

    Fix class names in module init.

--- src/QuoteEngine/__init__.py
+++ src/QuoteEngine/__init__.py
@@ -2,7 +2,7 @@ from .Ingestor import Ingestor
 from .IngestorInterface import IngestorInterface
 from .QuoteModel import QuoteModel
 
-from .CSVImporter import CSVImporter
-from .DocxImporter import DocxImporter
-from .PDFImporter import PDFImporter
-from .TextImporter import TextImporter
+from .CSVIngestor import CSVIngestor
+from .DocxIngestor import DocxIngestor
+from .PDFIngestor import PDFIngestor
+from .TextIngestor import TextIngestor
