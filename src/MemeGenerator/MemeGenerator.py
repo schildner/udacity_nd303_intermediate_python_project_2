5c0ddfec591025bbfc0fe6e07061118b3085dd4c
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 14:02:43 +0100

    Convert generator to a proper class.

--- src/MemeGenerator/MemeGenerator.py
+++ src/MemeGenerator/MemeGenerator.py
@@ -5,32 +5,33 @@ from PIL import Image, ImageDraw, ImageFont
 class MemeGenerator():
     """A meme generator."""
 
+    def __init__(self, output_dir='memes'):
+        self.output_dir = output_dir
 
-@classmethod
-def make_meme(cls, img_path, text, author, width=500) -> str:
-    """Create a meme image with specified width and including text & author.
+    def make_meme(self, img_path, text, author, width=500) -> str:
+        """Create a meme image with specified width and including text & author.
 
-        str -- the file path to the output image.
-    """
-    img = Image.open(img_path)
-    original_width, original_height = img.size
+            str -- the file path to the output image.
+        """
+        img = Image.open(img_path)
+        original_width, original_height = img.size
 
-    if width > 500:
-        width = 500
+        if width > 500:
+            width = 500
 
-    # New height must respect the aspect ratio of the original image.
-    new_height = int(width * original_height / original_width)
-    img = img.crop((0, 0, width, new_height))
+        # New height must respect the aspect ratio of the original image.
+        new_height = int(width * original_height / original_width)
+        img = img.crop((0, 0, width, new_height))
 
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('./fonts/LilitaOne-Regular.ttf', size=20)
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('./fonts/LilitaOne-Regular.ttf', size=20)
 
-    if text is not None:
-        draw.text((10, 30), text, font=font, fill='white')
+        if text is not None:
+            draw.text((10, 30), text, font=font, fill='white')
 
-    if author is not None:
-        draw.text((10, 60), text, font=font, fill='yellow')
+        if author is not None:
+            draw.text((10, 60), text, font=font, fill='yellow')
 
-    meme_path = f'./memes/{randint(0,1000000)}.jpg'
-    img.save(meme_path)
-    return meme_path
+        meme_path = f'./{self.output_dir}/{randint(0,1000000)}.jpg'
+        img.save(meme_path)
+        return meme_path
