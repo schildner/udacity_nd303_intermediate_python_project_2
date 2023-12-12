c995ab6f194b4f67b9b5361f6aac1b75084b5f8b
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 11:56:54 +0100

    Add module initialization.

---
+++ src/QuoteEngine/__init__.py
@@ -0,0 +1,8 @@
+from .Ingestor import Ingestor
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+
+from .CSVImporter import CSVImporter
+from .DocxImporter import DocxImporter
+from .PDFImporter import PDFImporter
+from .TextImporter import TextImporter
