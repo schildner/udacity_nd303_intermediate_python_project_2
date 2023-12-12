9818ea5549fc3e9b5a4ecd0afc4c5db2120e341b
Eduard Schildner <eduard.schildner@gmail.com>
2023-12-12 13:59:46 +0100

    Add drawing text on image.

--- src/MemeGenerator/MemeGenerator.py
+++ src/MemeGenerator/MemeGenerator.py
@@ -1,8 +1,13 @@
 from random import randint
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 
-def make_meme(self, img_path, text, author, width=500) -> str:
+class MemeGenerator():
+    """A meme generator."""
+
+
+@classmethod
+def make_meme(cls, img_path, text, author, width=500) -> str:
     """Create a meme image with specified width and including text & author.
 
         str -- the file path to the output image.
@@ -17,7 +22,14 @@ def make_meme(self, img_path, text, author, width=500) -> str:
     new_height = int(width * original_height / original_width)
     img = img.crop((0, 0, width, new_height))
 
-    img = img.crop((0, 0, width, new_height))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('./fonts/LilitaOne-Regular.ttf', size=20)
+
+    if text is not None:
+        draw.text((10, 30), text, font=font, fill='white')
+
+    if author is not None:
+        draw.text((10, 60), text, font=font, fill='yellow')
 
     meme_path = f'./memes/{randint(0,1000000)}.jpg'
     img.save(meme_path)
